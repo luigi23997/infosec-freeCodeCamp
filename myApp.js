54d94bf10202b8f9b5400dcc0bdef50544fe7177
root <root@kali.mydomani.net>
2024-09-12 12:58:19 +0200

    Add inital application

--- myApp.js
+++ myApp.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3030;
+
+
+app.listen(PORT, () => {
+  console.log('server started on port ${PORT}');
+});
 
 
 
