3dcf8bfbf4a6911537b10353f040f5bfbe5c46a5
root <root@kali.mydomani.net>
2024-09-12 22:49:45 +0200

    add helmet.hsts

--- myApp.js
+++ myApp.js
@@ -12,6 +12,7 @@ app.use(helmet.ieNoOpen({}));
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives: {defaultSrc: ["'self'"], scriptSrc: ["'self'", "trusted-cdn.com"]}}));
 
 
 module.exports = app;
