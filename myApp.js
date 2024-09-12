045b8496060fa658b34fa46b60edd79e97e89b02
root <root@kali.mydomani.net>
2024-09-12 22:38:28 +0200

    add helmet.hsts

--- myApp.js
+++ myApp.js
@@ -10,6 +10,7 @@ app.use(helmet.xssFilter({}));
 app.use(helmet.noSniff({}));
 app.use(helmet.ieNoOpen({}));
 app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
 
 
 module.exports = app;
