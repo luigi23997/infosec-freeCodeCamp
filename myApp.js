7d1b21e79ad805f1bec0a45e8c2f13860a4715c3
root <root@kali.mydomani.net>
2024-09-12 13:52:28 +0200

    use yarn and fix errors

--- myApp.js
+++ myApp.js
@@ -1,53 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
-const PORT = process.env.PORT || 3030;
-
-
-app.listen(PORT, () => {
-  console.log('server started on port ${PORT}');
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
